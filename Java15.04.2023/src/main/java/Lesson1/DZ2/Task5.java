ea3ade383d17b0a2d4fbdd77f3737ee8e3e2ca8d
Anton345653 <78899877+Anton345653@users.noreply.github.com>
2023-04-23 16:44:10 +0300

    Add files via upload

---
+++ Java15.04.2023/src/main/java/Lesson1/DZ2/Task5.java
@@ -0,0 +1,19 @@
+package Lesson1.DZ2;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int[] a = {4, 6, 8, 1};
+        int min = a[0];
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
+            } else if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+
+    }
+}
