c064917c0c9e823ec44eb3c209fc8270b3f8579a
Anton345653 <78899877+Anton345653@users.noreply.github.com>
2023-04-23 14:49:22 +0300

    Delete Task7.java

--- Task7.java
+++
@@ -1,9 +0,0 @@
-public class Task7 {
-    public static void main(String[] args) {
-        fuu("Bob");
-    }
-
-    public static void fuu(String name) {
-        System.out.println("Привет, " + name);
-    }
-}
